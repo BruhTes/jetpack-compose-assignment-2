31af18b049e07b28499042b0f9b9cb447ec95660
BruhTes <bruhtesfahun21129@gmail.com>
2025-05-18 23:53:16 +0300

    data model for remote fetch

---
+++ app/src/main/java/com/example/jetpack_compose_assignment_2/data/remote/TodoApi.kt
@@ -0,0 +1,8 @@
+package com.example.jetpack_compose_assignment_2.data.remote
+
+import retrofit2.http.GET
+
+interface TodoApi {
+    @GET("todos")
+    suspend fun getTodos(): List<RemoteTodoDto>
+} 
